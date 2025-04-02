0d52b7cc38e52f9cf2fb30b95a7ceca03e4a7164
Maziyo <wlgp5462@naver.com>
2025-04-03 05:37:26 +0900

    collect semifinish

--- src/pages/collect.jsx
+++ src/pages/collect.jsx
@@ -11,26 +11,22 @@ function Collect() {
 
     return (
         <>
-            <div className="main">
-                <div id="main01">
-                    <div className="main01-left">
+            <div id="main">
+                <div className="main01">
+                    <div id="main01-con">
                         <p className="recording">기록하기</p>
                         <h1 className="question">가장 기억에 남는<br />선택의 순간은<br />언제인가요?</h1>
                     </div>
-                    <p className="main01-right">기억의 도서관</p>
                 </div>
 
-                <div id="book-spine">
+                <div id="main02">
                     <textarea
                         className="userInput"
                         type="text"
                         placeholder="샘플 테스트"></textarea>
-
-                    <p id="month">4월 어느날의 몇번째 질문</p>
                 </div>
 
             </div>
-
 
             <div className="btn" >
                 <form onSubmit={handleClick}>
